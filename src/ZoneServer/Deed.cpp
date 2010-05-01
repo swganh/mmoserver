a5ef619150ece810f520c934d847c3f1f655b165
powerking <richardwlaughlin@gmail.com>
2010-05-01 13:49:05 -0400

    Fixed a mistake.

--- src/ZoneServer/Deed.cpp
+++ src/ZoneServer/Deed.cpp
@@ -50,14 +50,6 @@ void Deed::handleObjectMenuSelect(uint8 messageType,Object* srcObject)
 		{
 			case radId_itemUse:
 			{
-				//todo : check if the type of building is allowed on the planet
-
-				//check the region whether were allowed to build
-				if(!gStructureManager->checkCityRadius(player))
-				{
-					gMessageLib->sendSystemMessage(player,L"You cannot place this structure inside a no-build zone.");
-					return;
-				}
 
 				if(this->getItemType() >= ItemType_Deed_X34 && this->getItemType() <= ItemType_Deed_Swoop) //landspeeder x34, speederbike, swoop
 				{
@@ -81,6 +73,15 @@ void Deed::handleObjectMenuSelect(uint8 messageType,Object* srcObject)
 				}
 				else
 				{
+					//todo : check if the type of building is allowed on the planet
+
+					//check the region whether were allowed to build
+					if(!gStructureManager->checkCityRadius(player))
+					{
+						gMessageLib->sendSystemMessage(player,L"You cannot place this structure inside a no-build zone.");
+						return;
+					}
+
 					//enter deed placement mode
 					StructureDeedLink* data = gStructureManager->getDeedData(this->getItemType());
 					if(!data)

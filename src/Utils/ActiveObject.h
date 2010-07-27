1eebfad9fa530f83b040d68297a078c386a762ec
apathy <apathy@swganh.org>
2010-07-27 04:47:17 -0400

    Initial implementation of an ActiveObject utility that can be easily reused by objects to make them concurrent.
    Added a test to display how ActiveObject can be used.

---
+++ src/Utils/ActiveObject.h
@@ -0,0 +1,62 @@
+/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+Use of this source code is governed by the GPL v3 license that can be found
+in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+#ifndef SRC_UTILS_ACTIVEOBJECT_H_
+#define SRC_UTILS_ACTIVEOBJECT_H_
+
+#include <functional>
+#include <memory>
+
+#include "Utils/ConcurrentQueue.h"
+
+namespace boost {
+    class thread;
+}
+
+namespace utils {
+
+class ActiveObject {
+public:
+    typedef std::function<void()> Message;
+
+public:
+    ActiveObject();
+    ~ActiveObject();
+
+    void Send(Message message);
+
+private:
+    ::utils::ConcurrentQueue<Message> message_queue_;
+    std::unique_ptr<boost::thread> thread_;
+    bool done_;
+
+    void Run();
+};
+
+}
+
+#endif  // SRC_UTILS_ACTIVEOBJECT_H_

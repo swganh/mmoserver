65e34ca0a1a474051b3ae6b346f5419c84d4491b
apathy <apathy@swganh.org>
2010-08-04 02:15:43 -0400

    Removed unnecessary include from crc.h

--- src/Common/Crc.cpp
+++ src/Common/Crc.cpp
@@ -25,8 +25,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ---------------------------------------------------------------------------------------
 */
 
-#include "Common/EventDispatcher.h"
+#include "Common/Crc.h"
 
+#include <cstdint>
 #include <cstring>
 #include <vector>
 

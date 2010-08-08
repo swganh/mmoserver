3f99a623a1f1b1fd18cbf0a6d65c98781310fbe0
apathy <apathy@swganh.org>
2010-08-08 01:40:33 -0400

    Added a MockEvent object to descend from the refactored BaseEvent class.

---
+++ tests/Common/MockObjects/MockEvent.cpp
@@ -0,0 +1,3 @@
+#include "MockEvent.h"
+
+const ::common::EventType MockEvent::event_type_ = ::common::EventType("mock_event");
---
+++ tests/Common/MockObjects/MockEvent.h
@@ -0,0 +1,55 @@
+/*
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+Use of this source code is governed by the GPL v3 license that can be found
+in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+#ifndef SRC_COMMON_MOCKOBJECTS_MOCKEVENT_H_
+#define SRC_COMMON_MOCKOBJECTS_MOCKEVENT_H_
+
+#include "Common/Event.h"
+
+class MockEvent : public ::common::BaseEvent {
+public:
+    explicit MockEvent(uint64_t subject_id = 0, uint64_t timestamp = 0) 
+        : BaseEvent(subject_id, timestamp) 
+    , some_event_val_(0) {}
+    
+    explicit MockEvent(int some_event_val, uint64_t subject_id = 0, uint64_t timestamp = 0) 
+        : BaseEvent(subject_id, timestamp)
+    , some_event_val_(some_event_val) {}
+    
+    ~MockEvent() {}
+
+    const ::common::EventType& event_type() const { return event_type_; }
+
+    int some_event_val() const { return some_event_val_; }
+
+private:
+    void onSerialize(::common::ByteBuffer& out) const {
+        out << some_event_val_;
+    }
+
+    static const ::common::EventType event_type_;
+    int some_event_val_;
+};
+
+#endif  // SRC_COMMON_MOCKOBJECTS_MOCKEVENT_H_

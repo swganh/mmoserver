c4e969d06cd607e602f5eeab153beb440628e40a
Kronos <kronos@swganh.org>
2010-08-22 21:32:54 -0700

    small changes

--- src/ZoneServer/AbstractState.h
+++ src/ZoneServer/AbstractState.h
@@ -25,9 +25,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ---------------------------------------------------------------------------------------
 */
 #include "CreatureObject.h"
-//#include "StatemManager"
-
-//#define list<AbstractState*> TransitionList;
 
 #pragma once
 class IState

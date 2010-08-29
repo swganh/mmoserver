f3a7e6033e4cc2ffe6be4c82c3b0ddec589d1ff4
Kronos <kronos@swganh.org>
2010-08-28 23:53:19 -0700

    added locmotion files...'

---
+++ src/ZoneServer/LocomotionState.cpp
@@ -0,0 +1,142 @@
+/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+Use of this source code is governed by the GPL v3 license that can be found
+in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+#include "LocomotionState.h"
+
+
+LocomotionState::LocomotionState(void)
+{
+    mStateID = 0;
+    mTransitionList.clear();
+}
+
+LocomotionState::~LocomotionState(void)
+{
+}
+
+void LocomotionState::Enter(CreatureObject* obj)
+{
+    obj->setLocomotion(mStateID);
+}
+void LocomotionState::Exit(CreatureObject* obj)
+{
+    obj->setLocomotion(0);
+}
+bool LocomotionState::CanTransition(uint64 newLocomotionState)
+{
+    return true;
+}
+
+LocomotionStanding::LocomotionStanding()
+{
+    mStateID = CreatureLocomotion_Standing;
+}
+LocomotionSneaking::LocomotionSneaking()
+{
+    mStateID = CreatureLocomotion_Sneaking;
+
+}
+LocomotionWalking::LocomotionWalking()
+{
+    mStateID = CreatureLocomotion_Walking;
+}
+LocomotionRunning::LocomotionRunning()
+{
+    mStateID = CreatureLocomotion_Running;
+}
+LocomotionKneeling::LocomotionKneeling()
+{
+    mStateID = CreatureLocomotion_Kneeling;
+}
+LocomotionCrouchSneaking::LocomotionCrouchSneaking()
+{
+    mStateID = CreatureLocomotion_CrouchSneaking;
+}
+LocomotionCrouchWalking::LocomotionCrouchWalking()
+{
+    mStateID = CreatureLocomotion_CrouchWalking;
+}
+LocomotionProne::LocomotionProne()
+{
+    mStateID = CreatureLocomotion_Prone;
+}
+LocomotionCrawling::LocomotionCrawling()
+{
+    mStateID = CreatureLocomotion_Crawling;
+}
+LocomotionClimbingStationary::LocomotionClimbingStationary()
+{
+    mStateID = CreatureLocomotion_ClimbingStationary;
+}
+LocomotionClimbing::LocomotionClimbing()
+{
+    mStateID = CreatureLocomotion_Climbing;
+}
+LocomotionHovering::LocomotionHovering()
+{
+    mStateID = CreatureLocomotion_Hovering;
+}
+LocomotionFlying::LocomotionFlying()
+{
+    mStateID = CreatureLocomotion_Flying;
+}
+LocomotionLyingDown::LocomotionLyingDown()
+{
+    mStateID = CreatureLocomotion_LyingDown;
+}
+LocomotionSitting::LocomotionSitting()
+{
+    mStateID = CreatureLocomotion_Sitting;
+}
+LocomotionSkillAnimating::LocomotionSkillAnimating()
+{
+    mStateID = CreatureLocomotion_SkillAnimating;
+}
+LocomotionDrivingVehicle::LocomotionDrivingVehicle()
+{
+    mStateID = CreatureLocomotion_DrivingVehicle;
+}
+LocomotionRidingCreature::LocomotionRidingCreature()
+{
+    mStateID = CreatureLocomotion_RidingCreature;
+}
+LocomotionKnockedDown::LocomotionKnockedDown()
+{
+    mStateID = CreatureLocomotion_KnockedDown;
+}
+LocomotionIncapacitated::LocomotionIncapacitated()
+{
+    mStateID = CreatureLocomotion_Incapacitated;
+}
+LocomotionDead::LocomotionDead()
+{
+    mStateID = CreatureLocomotion_Dead;
+}
+LocomotionBlocking::LocomotionBlocking()
+{
+    mStateID = CreatureLocomotion_Blocking;
+}
---
+++ src/ZoneServer/LocomotionState.h
@@ -0,0 +1,254 @@
+/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2010 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+Use of this source code is governed by the GPL v3 license that can be found
+in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+#pragma once
+#ifndef ANH_ZONESERVER_LOCOMOTION_STATE_H
+#define ANH_ZONESERVER_LOCOMOTION_STATE_H
+
+#include "AbstractState.h"
+#include "CreatureObject.h"
+
+class LocomotionState :
+    public IState
+{
+public:
+    LocomotionState(void);
+    virtual ~LocomotionState(void);
+    /* Activates the Enter process for the character Locomotion
+    *  
+    */
+    virtual void Enter(CreatureObject* obj);
+    /* Activates the Exit process for the given Locomotion
+    *  
+    */
+    virtual void Exit(CreatureObject* obj);
+    /* Determines if the player can transition to the Locomotion
+    *  
+    */
+    virtual bool CanTransition(uint64 newLocomotionState);
+protected:
+    transitionList  mTransitionList;
+};
+#endif
+/* Standing Locomotion
+*
+*/
+class LocomotionStanding:
+    public LocomotionState
+{
+public:
+    LocomotionStanding();
+};
+/*	Sneaking Locomotion
+*
+*/
+class LocomotionSneaking:
+    public LocomotionState
+{
+public:
+    LocomotionSneaking();
+};
+/*	Walking Locomotion
+*
+*/
+class LocomotionWalking:
+    public LocomotionState
+{
+public:
+    LocomotionWalking();
+};
+/*	Running Locomotion
+*
+*/
+class LocomotionRunning:
+    public LocomotionState
+{
+public:
+    LocomotionRunning();
+};
+/*  Kneeling Locomotion
+*
+*/
+class LocomotionKneeling:
+    public LocomotionState
+{
+public:
+    LocomotionKneeling();
+};
+/*	CrouchSneaking Locomotion
+*
+*/
+class LocomotionCrouchSneaking:
+    public LocomotionState
+{
+public:
+    LocomotionCrouchSneaking();
+};
+/*	CrouchWalking Locomotion
+*
+*/
+class LocomotionCrouchWalking:
+    public LocomotionState
+{
+public:
+    LocomotionCrouchWalking();
+};
+/*	Prone Locomotion
+*
+*/
+class LocomotionProne:
+    public LocomotionState
+{
+public:
+    LocomotionProne();
+};
+/*	Crawling Locomotion
+*
+*/
+class LocomotionCrawling:
+    public LocomotionState
+{
+public:
+    LocomotionCrawling();
+};
+/*	ClimbingStationary Locomotion
+*
+*/
+class LocomotionClimbingStationary:
+    public LocomotionState
+{
+public:
+    LocomotionClimbingStationary();
+};
+/*	Climbing Locomotion
+*
+*/
+class LocomotionClimbing:
+    public LocomotionState
+{
+public:
+    LocomotionClimbing();
+};
+/*	Hovering Locomotion
+*
+*/
+class LocomotionHovering:
+    public LocomotionState
+{
+public:
+    LocomotionHovering();
+};
+/*	Flying Locomotion
+*
+*/
+class LocomotionFlying:
+    public LocomotionState
+{
+public:
+    LocomotionFlying();
+};
+/*	LyingDown Locomotion
+*
+*/
+class LocomotionLyingDown:
+    public LocomotionState
+{
+public:
+    LocomotionLyingDown();
+};
+/*	Sitting Locomotion
+*
+*/
+class LocomotionSitting:
+    public LocomotionState
+{
+public:
+    LocomotionSitting();
+};
+/*	SkillAnimating Locomotion
+*
+*/
+class LocomotionSkillAnimating:
+    public LocomotionState
+{
+public:
+    LocomotionSkillAnimating();
+};
+/*	DrivingVehicle Locomotion
+*
+*/
+class LocomotionDrivingVehicle:
+    public LocomotionState
+{
+public:
+    LocomotionDrivingVehicle();
+};
+/*	RidingCreature Locomotion
+*
+*/
+class LocomotionRidingCreature:
+    public LocomotionState
+{
+public:
+    LocomotionRidingCreature();
+};
+/*	KnockedDown Locomotion
+*
+*/
+class LocomotionKnockedDown:
+    public LocomotionState
+{
+public:
+    LocomotionKnockedDown();
+};
+/*	Incapacitated Locomotion
+*
+*/
+class LocomotionIncapacitated:
+    public LocomotionState
+{
+public:
+    LocomotionIncapacitated();
+};
+/*	Dead Locomotion
+*
+*/
+class LocomotionDead:
+    public LocomotionState
+{
+public:
+    LocomotionDead();
+};
+/*	Blocking Locomotion
+*
+*/
+class LocomotionBlocking:
+    public LocomotionState
+{
+public:
+    LocomotionBlocking();
+};

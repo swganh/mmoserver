178b11d063ff40cee81b86ec368d9c6f3811bd58
apathyboy <eric.barr@anhstudios.com>
2010-10-03 15:36:15 -0400

    Can only pass basic types through variadic parameters.

--- src/ZoneServer/CraftingSession.cpp
+++ src/ZoneServer/CraftingSession.cpp
@@ -365,7 +365,7 @@ void CraftingSession::handleObjectReady(Object* object,DispatchClient* client)
 
             if(mDraftSchematic)
             {
-                gLogger->log(LogManager::CRITICAL,"CraftingSession::handleObjectReady: DraftSchematic : %s / Object : %I64u",mDraftSchematic->getModel(),object->getId());
+                gLogger->log(LogManager::CRITICAL,"CraftingSession::handleObjectReady: DraftSchematic : %s / Object : %I64u",mDraftSchematic->getModel().getAnsi(), object->getId());
                 gLogger->log(LogManager::CRITICAL,"CraftingSession::handleObjectReady: DraftSchematic batch: %4u",mDraftSchematic->getWeightsBatchId());
             }
             mManufacturingSchematic = NULL;

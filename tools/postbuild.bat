3dd2ec2ef39e586c1179548214abe7a459c9500a
Eric Barr <eric.barr@anhstudios.com>
2010-10-23 14:05:30 -0400

    Added tbb and mysqlcppconn dll loading.

--- tools/postbuild.bat
+++ tools/postbuild.bat
@@ -15,6 +15,24 @@ if not exist %1bin\%2\libmysql.dll (
     xcopy %1deps\mysql\bin %1bin\%2 /I /Y /s
 )
 
+if not exist %1bin\%2\mysqlcppconn.dll (
+    xcopy %1deps\mysql-connector-cpp\driver\%2 %1bin\%2 /I /Y /s
+)
+
+if %2 == "Debug" (
+    if not exist %1bin\%2\tbb_debug.dll (
+        xcopy %1deps\tbb\bin\ia32\vc10\tbb_debug.dll %1bin\%2 /I /Y /s
+        xcopy %1deps\tbb\bin\ia32\vc10\tbb_debug.pdb %1bin\%2 /I /Y /s
+    )
+)
+
+if %2 == "Release" (    
+    if not exist %1bin\%2\tbb.dll (
+        xcopy %1deps\tbb\bin\ia32\vc10\tbb.dll %1bin\%2 /I /Y /s
+        xcopy %1deps\tbb\bin\ia32\vc10\tbb.pdb %1bin\%2 /I /Y /s
+    )
+)
+
 if not exist %1bin\%2\config (
     xcopy %1data\config %1bin\%2\config /I /Y /s
 )

bf00d85b2ab637f0ccb523b9d07dbad982daf33c
Eric Barr <eric.barr@anhstudios.com>
2010-11-03 05:07:25 -0700

    Updated cmake build files to install built applications to the appropriate bin directory

--- data/server_start.sh
+++ data/server_start.sh
@@ -2,31 +2,31 @@
 
 #conn=`ps aux | grep -v grep | grep connserver`
 #if [! conn]; then
-    nohup ./src/ConnectionServer/connserver &
+    nohup ./src/ConnectionServer/ConnectionServer &
 #fi
 #
 #chat=`ps aux | grep -v grep | grep chatserver`
 #if [! chat]; then
-    nohup ./src/ChatServer/chatserver &
+    nohup ./src/ChatServer/ChatServer &
 #fi
 #
 #login=`ps aux | grep -v grep | grep loginserver`
 #if [! login]; then
-    nohup ./src/LoginServer/loginserver &
+    nohup ./src/LoginServer/LoginServer &
 #fi
 #
 #ping=`ps aux | grep -v grep | grep pingserver`
 #if [! ping]; then
-    nohup ./src/PingServer/pingserver &
+    nohup ./src/PingServer/PingServer &
 #fi
 #
 #tutorial=`ps aux | grep -v grep | grep tutorial`
 #if [! tutorial]; then
-#    nohup ./src/ZoneServer/zoneserver tutorial
+#    nohup ./src/ZoneServer/ZoneServer tutorial
 #fi
 #
 #tatooine=`ps aux | grep -v grep | grep tatooine`
 #if [! tatooine]; then
-#    nohup ./src/ZoneServer/zoneserver tatooine &
+#    nohup ./src/ZoneServer/ZoneServer tatooine &
 #fi
 clear

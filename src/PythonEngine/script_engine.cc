9ad872455a3d7549ab997e6fb3b85c6f2c6f3c34
Kyle Craviotto <kronos@swganh.org>
2011-01-03 20:18:56 -0800

    added renamed files back

---
+++ src/PythonEngine/script_engine.cc
@@ -0,0 +1,156 @@
+#include "script_engine.h"
+#include <iostream>
+#include "anh/event_dispatcher/event_dispatcher.h"
+
+using namespace python;
+script_engine::script_engine(const std::string& base_path)
+{
+    base_path_ = base_path;
+    // initialize the python script_engine
+    Py_Initialize();
+}
+script_engine::~script_engine()
+{
+    loaded_files_.empty();
+}
+void script_engine::load(const std::string& filename)
+{
+    // what this does is first calls the Python C-API to load the file, then pass the returned
+    // PyObject* into handle, which takes reference and sets it as a boost::python::object.
+    // this takes care of all future referencing and dereferencing.
+    try{
+        bp::object file_object(bp::handle<>(PyFile_FromString(fullPath(filename), "r" )));
+        loaded_files_.insert(std::make_pair(std::string(fullPath(filename)), file_object));
+    }
+    catch(...)
+    {
+        getExceptionFromPy();
+    }
+}
+void script_engine::run(const std::string& filename)
+{
+    // are you trying to run a file that's not loaded?
+    // lets load the file and run it anyway
+    if (!isFileLoaded(filename))
+        load(filename);
+
+    bp::object loaded_file = getLoadedFile(filename);
+    try
+    {
+        PyRun_SimpleFile( PyFile_AsFile( loaded_file.ptr()), fullPath(filename) );
+    }
+    catch(...)
+    {
+        getExceptionFromPy();
+    }
+}
+void script_engine::reload(const std::string& filename)
+{
+    if (isFileLoaded(filename))
+    {
+        removeFile(filename);
+    }
+    load(filename);
+}
+void script_engine::removeFile(const std::string& filename)
+{
+    auto it = loaded_files_.begin();
+    for (; it != loaded_files_.end();)
+    {
+        if (it->first == fullPath(filename))
+        {
+            loaded_files_.erase(it++);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
+bool script_engine::isFileLoaded(const std::string& filename)
+{
+    auto it = std::find_if(loaded_files_.begin(), loaded_files_.end(), [this,&filename](bp_object_map::value_type& file){
+        return file.first == fullPath(filename);
+    });
+    return it != loaded_files_.end();
+}
+void script_engine::setFullPath(const std::string& filename)
+{
+    setFullPath(filename, base_path_);
+}
+void script_engine::setFullPath(const std::string& filename, const std::string& root_path)
+{
+    full_path_.clear();
+    full_path_.append(root_path);
+    full_path_.append(filename);
+}
+bp::object script_engine::getLoadedFile(const std::string& filename)
+{
+    auto it = std::find_if(loaded_files_.begin(), loaded_files_.end(), [&](bp_object_map::value_type& file){
+        return file.first == fullPath(filename);
+    });
+    if (it != loaded_files_.end())
+        return it->second;
+    else 
+        return bp::object();
+}
+char* script_engine::fullPath(const std::string& filename)
+{
+    setFullPath(filename);
+    return const_cast<char*>(full_path_.c_str());
+}
+void script_engine::getExceptionFromPy()
+{
+    
+    PyObject* type, *value, *trace_back;
+    PyErr_Fetch(&type, &value, &trace_back);
+    // normalize to change value from tuple to string
+    PyErr_NormalizeException(&type, &value, &trace_back);
+    //value contains error message
+    //trace_back contains stack snapshot and other information
+    //(see python traceback structure)
+    try
+    {
+        if (trace_back) {
+            bp::handle<> hTraceback(trace_back);
+            bp::object traceback(hTraceback);
+
+            //Extract line number (top entry of call stack)
+            // if you want to extract another levels of call stack
+            // also process traceback.attr("tb_next") recurently
+            py_exception.line_num = bp::extract<std::string> (traceback.attr("tb_lineno"));
+            py_exception.file_name = bp::extract<std::string>(traceback.attr("tb_frame").attr("f_code").attr("co_filename"));
+            py_exception.func_name = bp::extract<std::string>(traceback.attr("tb_frame").attr("f_code").attr("co_name"));
+        }
+        if (value)
+        {
+            //Extract error message
+            bp::handle<> hVal (PyObject_Str(value));
+            bp::object err_msg(hVal);
+            py_exception.err_msg = bp::extract<std::string>(err_msg);
+        }
+    } 
+    catch(...)
+    {
+        return;
+    }
+}
+std::string script_engine::getErrorMessage()
+{
+    if (py_exception.err_msg.length() > 0)
+    {
+        std::stringstream ss;
+        ss << "Error: " << py_exception.err_msg;
+        if (py_exception.line_num.length() >0)
+        {
+            ss << " On Line: " << py_exception.line_num;
+            ss << " in file: " << py_exception.file_name;
+            ss << " function: " << py_exception.func_name;
+        }
+        return ss.str();
+    }
+    else
+    {
+        return "Undefined Error";
+    }
+}
---
+++ src/PythonEngine/script_engine.h
@@ -0,0 +1,132 @@
+#ifndef _PYTHON_SCRIPT_ENGINE_H_
+#define _PYTHON_SCRIPT_ENGINE_H_
+
+#include <boost/python.hpp>
+#include <map>
+#include <vector>
+
+namespace python{
+namespace bp = boost::python;
+
+typedef std::map<std::string,bp::object> bp_object_map;
+class object;
+/*! Assists the user in loading and executing Python scripts from
+*   within C++.
+*
+*/
+class script_engine
+{
+public:
+    /**
+    * \brief creates script_engine with the base path specified
+    *
+    * \param base_path sets where the engine looks for the files
+    */
+    script_engine(const std::string& base_path);
+    ~script_engine();
+    /**
+    * \brief load's the python script into memory
+    *  and stores the Boost Python Object into a 
+    *  private map for later use in @runLoadedFile
+    *
+    * \param filename The file to load into memory
+    */
+    void load(const std::string& filename);
+
+     /**
+    * \brief run's the python file
+    *
+    * \param filename The file to run from the loaded_files_ map
+    *   if not found, a message will be given
+    */
+    void run(const std::string& filename);
+
+     /**
+    * \brief reload's the python file
+    *
+    * \param filename The file to remove from the loaded_files_ map
+    *  the file will be (re)loaded
+    */
+    void reload(const std::string& filename);
+
+    /**
+    * \brief remove the python file
+    *
+    * \param filename The file to remove from the loaded_files_ map
+    *   if not found, no action occurs
+    */
+    void removeFile(const std::string& filename);
+
+    /**
+    * \brief outputs the currently loaded files available
+    *   for immediate execution.
+    *
+    */
+    bp_object_map getLoadedFiles() { return loaded_files_; }
+
+    /**
+    * \brief checks to see if the filename has been loaded
+    * 
+    * \returns true if the file has been loaded, false else.
+    */
+    bool isFileLoaded(const std::string& filename);
+
+    /**
+    * \brief sets the default file path to find scripts
+    *  
+    * \param filepath sets the path_ behind the scenes
+    */
+    bp::object getLoadedFile(const std::string& filename);
+    /**
+    * \brief gets data from PYEXCEPTION struct and 
+    *       creates a friendly message
+    *  
+    */
+    std::string getErrorMessage();
+private:
+    // hide default ctor
+    script_engine();
+    // used by internal functions
+    void setFullPath(const std::string& filename, const std::string& root_path);
+    void setFullPath(const std::string& filename);
+    /**
+    * \brief sets the PYEXCEPTION struct based on internal python values
+    *
+    * \effect struct is filled out
+    */
+    void getExceptionFromPy();
+    // base path set by the default ctor
+    std::string base_path_;
+    /**
+    * \brief helper function to take in base filename and return full path
+    *
+    * \param filename to add to base path
+    * \returns char* of full path after adding filename
+    */
+    char* fullPath(const std::string& filename);
+
+    /**
+    * \brief full path including .py
+    */
+    std::string full_path_;
+
+    /**
+    * \brief a std::map containing the name and boost python object
+    *   of all loaded files
+    */
+    bp_object_map loaded_files_; 
+
+    /**
+    * \brief struct containing last available python exception
+    */
+    struct PYEXCEPTION
+    {
+        std::string err_msg;
+        std::string line_num;
+        std::string file_name;
+        std::string func_name;
+    } py_exception;
+};
+
+} // namespace python
+#endif //_PYTHON_SCRIPT_ENGINE_H_

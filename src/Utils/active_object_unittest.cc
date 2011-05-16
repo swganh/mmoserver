716a8f0632151d65c85e63b85c376b439bef2bb9
Eric Barr <eb@anhstudios.com>
2011-05-16 04:07:32 -0400

    Copyright updated for 2011 and latest gpl v2 license

--- src/Utils/active_object_unittest.cc
+++ src/Utils/active_object_unittest.cc
@@ -1,6 +1,22 @@
-// Copyright (c) 2010 ApathyStudios. All rights reserved.
-// Use of this source code is governed by a GPL-style license that can be
-// found in the COPYING file.
+/*
+ This file is part of MMOServer. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2011 The SWG:ANH Team
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "Utils/ActiveObject.h"
 

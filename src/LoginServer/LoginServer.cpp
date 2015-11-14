801e35729b804819f9f503fd4232fff6faf20a5a
Obi-Two <johnroyuk@gmail.com>
2015-11-14 21:47:13 +0000

    remove usleep

--- src/LoginServer/LoginServer.cpp
+++ src/LoginServer/LoginServer.cpp
@@ -180,8 +180,8 @@ int main(int argc, char* argv[])
 				if(std::cin.get() == 'q')
 					break;
 
-			//boost::this_thread::sleep(boost::posix_time::milliseconds(10));
-			usleep(2000);
+			boost::this_thread::sleep(boost::posix_time::milliseconds(10));
+			//usleep(2000);
 		}
 
 		// Shutdown things
